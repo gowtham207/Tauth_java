3e432367b8ced84fa7923eee2f00f3411bc8a5b6
gowtham207 <gowthamkannan207@gmail.com>
2025-10-20 15:49:03 +0530

    added the signup screen

--- frontend/src/screens/SignupScreen.tsx
+++ frontend/src/screens/SignupScreen.tsx
@@ -3,7 +3,8 @@ import type React from "react"
 
 const SignupScreen: React.FC = () => {
   return (
-    <div>SignupScreen</div>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 p-4">
+SignupScreen</div>
   )
 }
 
